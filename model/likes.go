67db8db6a9d40ea16f35849e11e6917e33497743
xiaolin <lintingyong2futurebots.cn>
2018-06-11 22:22:09 +0800

    修改文件文件名 Likes > likes

--- model/likes.go
+++ model/likes.go
@@ -1,6 +1,5 @@
 package model
 
-
 import (
 	"time"
 	_ "github.com/jinzhu/gorm"
@@ -13,7 +12,8 @@ type Likes struct {
 	UpdatedAt time.Time  `json:"-"`
 	DeletedAt *time.Time `json:"-"`
 
-	UserId int64 `json:"userId"`
-	ByUserId int64 `json:"byUserId"` //点赞人的id
-
+	UserId   int64  `json:"userId"`
+	ByUserId int64  `json:"byUserId"` //点赞人的id
+	From     string `json:"from"`     //点赞出处，关联表名
+	FromId   int64  `json:"fromId"`   //相关出处id
 }
